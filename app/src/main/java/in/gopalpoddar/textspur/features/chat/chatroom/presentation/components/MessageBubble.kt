f2ba7a498e91173f965128511d1f7e4227e74dd9
Gopal Poddar <g43142324@gmail.com>
2026-08-01 21:41:36 +0530

    chat room ui fix

--- app/src/main/java/in/gopalpoddar/textspur/features/chat/chatroom/presentation/components/MessageBubble.kt
+++ app/src/main/java/in/gopalpoddar/textspur/features/chat/chatroom/presentation/components/MessageBubble.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -58,6 +60,7 @@ fun MessageBubble(
     ) {
         Column(
             modifier = Modifier
+                .widthIn(max = 300.dp)
                 .clip(shape)
                 .background(backgroundColor)
                 .padding(12.dp)
